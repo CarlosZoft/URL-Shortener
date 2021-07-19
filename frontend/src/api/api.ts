b8c36932d14b874ba6b5f97c37cc12410a769451
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-07-19 03:19:55 -0300

    refactor: update headers for use axios methods

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3030",
-  headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" },
+  baseURL: "http://localhost:3030/",
+  headers: {
+    "Content-type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+  },
 });
 
 export default api;
