c60d0156558a4a7abcf7c265d82448ac7a05418d
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-07-19 03:20:41 -0300

    feat: creating request in the extenal api - POST

--- frontend/src/api/createURL.ts
+++ frontend/src/api/createURL.ts
@@ -1,8 +1,12 @@
 import api from "./api";
 import { IBody } from "../interfaces";
 
-export async function createUrl(body: IBody) {
-  console.log(body);
-  const response = await api.post("/urls", { body });
+export async function createUrl({ fullUrl, validAcess, validDays }: IBody) {
+  const body = JSON.stringify({
+    fullUrl,
+    validAcess,
+    validDays,
+  });
+  const response = await api.post("urls", body);
   return response;
 }
