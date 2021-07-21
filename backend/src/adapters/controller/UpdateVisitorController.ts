fb3d178c55e15c3f834b1555300cb7e6ce6c9fbd
CarlosZoft <62192072+CarlosZoft@users.noreply.github.com>
2021-07-21 01:38:10 -0300

    refactor: set redirect for user prefer

--- backend/src/adapters/controller/UpdateVisitorController.ts
+++ backend/src/adapters/controller/UpdateVisitorController.ts
@@ -1,7 +1,7 @@
 import { UpdateVisitorService } from "../../service/UpdateVisitorService";
 import { ControllerInterface } from "../../presentation/protocols/ControllerInterface";
 import { Request, Response } from "express";
-import { MissingParamError, ServerError } from "../../presentation/error";
+import { MissingParamError } from "../../presentation/error";
 export class UpdateVisitorController implements ControllerInterface {
   async handle(request: Request, response: Response): Promise<any> {
     const { endpoint } = request.params;
@@ -11,8 +11,6 @@ export class UpdateVisitorController implements ControllerInterface {
     const service = new UpdateVisitorService();
     await service.linkExpires(endpoint);
     const data = await service.execute(endpoint);
-    if (data) {
-      response.status(200).send(data);
-    } else throw new ServerError();
+    response.redirect(data.fullUrl);
   }
 }
